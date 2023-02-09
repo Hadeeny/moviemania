433bd7f397ef9eb8b3d453e81aef6ef7c51c6c4e
hadeeny <oluwasegunadeniyi064@gmail.com>
2023-02-09 04:24:38 +0100

    padding adjustment

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className={`my-12 mx-28`}>
-      <h1 className='text-5xl text-green-500 my-8'>Movie<span className='text-cyan-700'>Mania</span></h1>
+      <body className={`my-12 mx-[2rem]`}>
+      <h1 className='text-3xl md:text-5xl text-green-500 my-8'>Movie<span className='text-cyan-700'>Mania</span></h1>
         {children}</body>
     </html>
   )
