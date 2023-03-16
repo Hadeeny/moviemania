9c0fbbf7604fed3a1a7facb43bd0895a4b485706
hadeeny <oluwasegunadeniyi064@gmail.com>
2023-03-16 22:04:06 +0100

    lol

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -28,22 +28,23 @@ export default async function MovieDetail({ params }) {
   
   
   const singleMovie = await film.find(movie => movie.Title === movieTitle);
+  const {Title:title, Year, Size, Category, CoverPhotoLink, Description, DownloadLink} = singleMovie
   return (
     <div>
       <div>
-        {/* <h1 className="text-2xl">{singleMovie.Title}</h1> */}
-        <h2 className="text-lg">{singleMovie.Year}</h2>
-        <h2 className="text-lg">Category: {singleMovie.Category}</h2>
-        <h2 className="text-lg">Download Size: {singleMovie.Size}</h2>
+        <h1 className="text-2xl">{title}</h1>
+        <h2 className="text-lg">{Year}</h2>
+        <h2 className="text-lg">Category: {Category}</h2>
+        <h2 className="text-lg">Download Size: {Size}</h2>
         <div className="my-12 w-full">
           <img
             width="1000px"
             height="1000px"
-            src={singleMovie.CoverPhotoLink}
+            src={CoverPhotoLink}
           />
         </div>
-        <p>{singleMovie.Description}</p>
-          <a href={singleMovie.DownloadLink} target="_blank" className="bg-green-400 px-4 py-1 text-sm rounded-md inline-block">
+        <p>{Description}</p>
+          <a href={DownloadLink} target="_blank" className="bg-green-400 px-4 py-1 text-sm rounded-md inline-block">
             Download
           </a>
         
