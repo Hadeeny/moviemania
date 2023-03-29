ed26cb44ebd320c56194dc532c4daf2e74a4ebac
hadeeny <oluwasegunadeniyi064@gmail.com>
2023-03-29 16:45:59 +0200

    fetch fix

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,33 +1,44 @@
+async function getData(title) {
+  // const res = await fetch('https://api.example.com/...');
+  const res = await fetch(
+    `https://tfvids-node.onrender.com/getData/?page=1&engine=nkiri,fzmovies`
+  );
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  const singleMovie = await data.find((movie) => movie.Title === title);
+  return singleMovie;
+}
+
 export default async function MovieDetail({ params }) {
   const { movie } = params;
- 
-  const movieTitle = movie.split('%20').join(' ')
-  const response = await fetch(`https://tfvids-node.onrender.com/getData/?page=1&engine=nkiri,fzmovies`)
-  const data = await response.json()
-  const singleMovie=  data.find(movie => movie.Title === movieTitle)
 
+  const movieTitle = movie.split("%20").join(" ");
+  const singleMovie = await getData(movieTitle);
 
-    
-  return ( 
+  return (
     <div className="mt-[4rem] w-full md:w-1/3 mx-auto text-center flex flex-col justify-center">
-          <div>
+      <div>
         <h1 className="text-2xl">{singleMovie.Title}</h1>
         <h2 className="text-lg">{singleMovie.Year}</h2>
         <h2 className="text-lg">Category: {singleMovie.Category}</h2>
         <h2 className="text-lg">Download Size: {singleMovie.Size}</h2>
         <div className="my-12 w-full">
-          <img
-          className="w-[14rem] mx-auto"
-            src={singleMovie.CoverPhotoLink}
-          />
+          <img className="w-[14rem] mx-auto" src={singleMovie.CoverPhotoLink} />
         </div>
         <p className="mb-4">{singleMovie.Description}</p>
-          <a download href={singleMovie.DownloadLink} target="_blank" className="bg-green-400 px-6 py-2 text-sm rounded-md inline-block">
-            Download
-          </a>
-        
+        <a
+          download
+          href={singleMovie.DownloadLink}
+          target="_blank"
+          className="bg-green-400 px-6 py-2 text-sm rounded-md inline-block"
+        >
+          Download
+        </a>
       </div>
-
     </div>
   );
 }
--- app/search/[searchquery]/page.jsx
+++ app/search/[searchquery]/page.jsx
@@ -1,41 +1,62 @@
 // 'use client'
-import { Link } from 'next/link'
-import React from 'react'
+import { Link } from "next/link";
+import React from "react";
 
-export default async function page({params}) {
-  const {searchquery }= params
-  const response = await fetch(`https://tfvids.onrender.com/search?query=${searchquery}&engine=fzmovies`)
-  const data = await response.json()
-  
-  console.log(data)
-  
-  return ( 
-    <div className='mt-[4rem]'> 
-      {data.length<=1? (<div className='text-center text-gray-400'>sorry ☹ no results were found 
-      for: <span className='text-red-300'>{searchquery}</span>, please try a new search or checkback later</div>): (
-        <>
-        <div className='text-center py-8 text-gray-400'>showing {data.length} results for: {searchquery}</div>
-      <div className='grid gap-16 grid-cols-fluid'>
-      {data.map((movie, index)=>(
-        <div 
-        key={index}
-        className='w-full flex flex-col gap-y-2 items-center'
-        >
-          <div className='font-semibold text-gray-100'>{movie.Title}</div>
-          <a href={`/singlemovie/${movie.Title}`}>
-            <img src={movie.CoverPhotoLink} alt={searchquery} />
-          </a>
-          <p>{movie.Category}</p> 
-          <p>{movie.Size}</p> 
-          <a href={movie.DownloadLink} target="_blank" className='bg-green-400 flex justify-center py-1 w-[10rem] rounded' download>
-            <span>Download</span>
-            {/* <img src={vercel} alt='download'/> */}
-          </a>
+async function getData(params) {
+  // const res = await fetch('https://api.example.com/...');
+  const res = await fetch(
+    `https://tfvids.onrender.com/search?query=${params}&engine=fzmovies`
+  );
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function page({ params }) {
+  const { searchquery } = params;
+  const data = await getData(searchquery);
+  return (
+    <div className="mt-[4rem]">
+      {data.length <= 1 ? (
+        <div className="text-center text-gray-400">
+          sorry ☹ no results were found for:{" "}
+          <span className="text-red-300">{searchquery}</span>, please try a new
+          search or checkback later
         </div>
-      ))}
-      </div>
+      ) : (
+        <>
+          <div className="text-center py-8 text-gray-400">
+            showing {data.length} results for: {searchquery}
+          </div>
+          <div className="grid gap-16 grid-cols-fluid">
+            {data.map((movie, index) => (
+              <div
+                key={index}
+                className="w-full flex flex-col gap-y-2 items-center"
+              >
+                <div className="font-semibold text-gray-100">{movie.Title}</div>
+                <a href={`/singlemovie/${movie.Title}`}>
+                  <img src={movie.CoverPhotoLink} alt={searchquery} />
+                </a>
+                <p>{movie.Category}</p>
+                <p>{movie.Size}</p>
+                <a
+                  href={movie.DownloadLink}
+                  target="_blank"
+                  className="bg-green-400 flex justify-center py-1 w-[10rem] rounded"
+                  download
+                >
+                  <span>Download</span>
+                </a>
+              </div>
+            ))}
+          </div>
         </>
       )}
-    </div> 
-  )
+    </div>
+  );
 }
