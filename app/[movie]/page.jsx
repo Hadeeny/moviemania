c822d97ddf1a94b360a76033bac1d35fd70a2442
hadeeny <oluwasegunadeniyi064@gmail.com>
2023-03-29 16:59:37 +0200

    fetch fix 2

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -21,24 +21,31 @@ export default async function MovieDetail({ params }) {
 
   return (
     <div className="mt-[4rem] w-full md:w-1/3 mx-auto text-center flex flex-col justify-center">
-      <div>
-        <h1 className="text-2xl">{singleMovie.Title}</h1>
-        <h2 className="text-lg">{singleMovie.Year}</h2>
-        <h2 className="text-lg">Category: {singleMovie.Category}</h2>
-        <h2 className="text-lg">Download Size: {singleMovie.Size}</h2>
-        <div className="my-12 w-full">
-          <img className="w-[14rem] mx-auto" src={singleMovie.CoverPhotoLink} />
+      {singleMovie == undefined || null || "undefined" ? (
+        <p>loading</p>
+      ) : (
+        <div>
+          <h1 className="text-2xl">{singleMovie.Title}</h1>
+          <h2 className="text-lg">{singleMovie.Year}</h2>
+          <h2 className="text-lg">Category: {singleMovie.Category}</h2>
+          <h2 className="text-lg">Download Size: {singleMovie.Size}</h2>
+          <div className="my-12 w-full">
+            <img
+              className="w-[14rem] mx-auto"
+              src={singleMovie.CoverPhotoLink}
+            />
+          </div>
+          <p className="mb-4">{singleMovie.Description}</p>
+          <a
+            download
+            href={singleMovie.DownloadLink}
+            target="_blank"
+            className="bg-green-400 px-6 py-2 text-sm rounded-md inline-block"
+          >
+            Download
+          </a>
         </div>
-        <p className="mb-4">{singleMovie.Description}</p>
-        <a
-          download
-          href={singleMovie.DownloadLink}
-          target="_blank"
-          className="bg-green-400 px-6 py-2 text-sm rounded-md inline-block"
-        >
-          Download
-        </a>
-      </div>
+      )}
     </div>
   );
 }
