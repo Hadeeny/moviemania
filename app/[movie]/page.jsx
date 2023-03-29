9e1096a1eded996710d015fcf3248beeb60723be
hadeeny <oluwasegunadeniyi064@gmail.com>
2023-03-29 21:13:08 +0200

    fetch

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -8,44 +8,37 @@ async function getData(title) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
   }
-  const data = await res.json();
-  const singleMovie = await data.find((movie) => movie.Title === title);
-  return singleMovie;
+  return res.json();
+  // const singleMovie = await data.find((movie) => movie.Title === title);
+  // return singleMovie;
 }
 
 export default async function MovieDetail({ params }) {
   const { movie } = params;
 
   const movieTitle = movie.split("%20").join(" ");
-  const singleMovie = await getData(movieTitle);
-
+  const movies = await getData();
+  const singleMovie = movies.find((movie) => movie.Title == movieTitle);
   return (
     <div className="mt-[4rem] w-full md:w-1/3 mx-auto text-center flex flex-col justify-center">
-      {singleMovie == undefined || null || "undefined" ? (
-        <p>loading</p>
-      ) : (
-        <div>
-          <h1 className="text-2xl">{singleMovie.Title}</h1>
-          <h2 className="text-lg">{singleMovie.Year}</h2>
-          <h2 className="text-lg">Category: {singleMovie.Category}</h2>
-          <h2 className="text-lg">Download Size: {singleMovie.Size}</h2>
-          <div className="my-12 w-full">
-            <img
-              className="w-[14rem] mx-auto"
-              src={singleMovie.CoverPhotoLink}
-            />
-          </div>
-          <p className="mb-4">{singleMovie.Description}</p>
-          <a
-            download
-            href={singleMovie.DownloadLink}
-            target="_blank"
-            className="bg-green-400 px-6 py-2 text-sm rounded-md inline-block"
-          >
-            Download
-          </a>
+      <div>
+        <h1 className="text-2xl">{singleMovie.Title}</h1>
+        <h2 className="text-lg">{singleMovie.Year}</h2>
+        <h2 className="text-lg">Category: {singleMovie.Category}</h2>
+        <h2 className="text-lg">Download Size: {singleMovie.Size}</h2>
+        <div className="my-12 w-full">
+          <img className="w-[14rem] mx-auto" src={singleMovie.CoverPhotoLink} />
         </div>
-      )}
+        <p className="mb-4">{singleMovie.Description}</p>
+        <a
+          download
+          href={singleMovie.DownloadLink}
+          target="_blank"
+          className="bg-green-400 px-6 py-2 text-sm rounded-md inline-block"
+        >
+          Download
+        </a>
+      </div>
     </div>
   );
 }
